ad2295ed11774b0074ac06cfab0df07f4813bf0a
Licarious <37857277+Licarious@users.noreply.github.com>
2022-03-15 19:26:47 -0700

    Initial commit

---
+++ BlankMapGenerator.py
@@ -0,0 +1,239 @@
+
+from PIL import Image
+import time
+import os
+
+
+#Impassible Sea, Sea, River, Lake, Wasteland/Impassable Mountains, Unihabitable, Impasable Terrain
+drawColorList = [(15,15,100),(30,30,150),(100,100,250),(150,150,200),(50,50,50),(185,185,185),(95,95,95)]
+
+
+
+
+class ProvinceDefinition:
+    id = 0
+    red = 0
+    green = 0
+    blue = 0
+    name = ""
+    name2 = ""
+    other_info = ""
+    lastKnownY = -1
+    def getRGBA(self):
+        return((self.red,self.green,self.blue,255))
+    def getRGB(self):
+        return((self.red,self.green,self.blue))
+
+
+
+provList = []
+mapDefinition = open("Input/definition.csv", encoding="utf8")
+def readProvinceDeff():
+    for province in mapDefinition:
+        if province.strip().startswith("#"):
+            pass
+        else:
+            tmpline = province.strip().split(';')
+            try:
+                province = ProvinceDefinition()
+                province.red = int(tmpline[1])
+                province.id = int(tmpline[0].lstrip("#"))
+                province.green = int(tmpline[2])
+                province.blue = int(tmpline[3])
+                province.name = tmpline[4]
+                provList.append(province)
+            except:
+                pass
+    pass
+
+
+
+def getRangeList(line, tmpList):
+    tmpline = line.replace("{", " ")
+    tmpline = tmpline.replace("}", " ")
+    tmpline = tmpline.replace("#", " # ")
+    words = tmpline.split(" ")
+    if "RANGE" in line:
+        x1=0
+        x2=0
+        for word in words:
+            if "#" in word:
+                break
+            else:
+                try:
+                    if x1 == 0:
+                        x1 = int(word)
+                    elif x2 == 0:
+                        x2 = int(word)
+                except:
+                    pass
+        for i in range(x1,x2+1):
+            tmpList.append(i)
+    elif "LIST" in line:
+        for word in words:
+            if "#" in word:
+                break
+            else:
+                try:
+                    tmpList.append(int(word))
+                except:
+                    pass
+    pass
+
+
+seaList = []
+riverList = []
+lakeList = []
+wastelandList = []
+uninhabitableList = []
+impasableterrainList = []
+impassableSeaList = []
+def parssDefaultMap():
+    defaultMap = open("Input/default.map")
+    for line in defaultMap:
+        if line.strip().startswith("#"):
+            pass
+        else:
+            if line.strip().lower().startswith("sea_zones"):
+                getRangeList(line, seaList)
+            if line.strip().lower().startswith("river_provinces"):
+                getRangeList(line, riverList)
+            if line.strip().lower().startswith("lakes"):
+                getRangeList(line, lakeList)
+            if line.strip().lower().startswith("wasteland") or line.strip().lower().startswith("impassable_mountains"):
+                getRangeList(line, wastelandList)
+            if line.strip().lower().startswith("uninhabitable"):
+                getRangeList(line, uninhabitableList)
+            if line.strip().lower().startswith("impassable_terrain"):
+                getRangeList(line, impasableterrainList)
+            if line.strip().lower().startswith("impassable_seas"):
+                getRangeList(line, impassableSeaList)
+    pass
+
+
+def drawMat(deffProvList):
+    if os.path.exists("error.txt"):
+    os.remove("error.txt")
+    try:
+        provMap = Image.open("Input/provinces.png")
+    except:
+        print("Unable to read provinces.png, please open and resave it.")
+        f = open("error.txt",'w')
+        f.write("Unable to read provinces.png, please open and resave it.")
+        f.close()
+    provMap.putalpha(255)
+    xRange= range(0,provMap.size[0],1)
+    yRange= range(0,provMap.size[1],1)
+    drawReader = provMap.load()
+    drawingMap = Image.new("RGB", (provMap.size[0],provMap.size[1]), color = "white")#Image.open("Input/provinces.png")
+    #drawingMap.putalpha(0)
+    riverMat = drawingMap.load()
+    z=0
+    dis = 5
+    
+
+    count = 0
+
+    print("Starting Compression")
+    tmpMapColor = []
+    ColorLength = []
+    for y in yRange:
+        mapLine = []
+        ColorlengthLine = []
+        length = 1
+        color = drawReader[0,y]
+        for x in range(1,provMap.size[0]):
+            if drawReader[x,y] == color:
+                length+=1
+            else:
+                mapLine.append(color)
+                ColorlengthLine.append(length)
+
+                length=1
+                color = drawReader[x,y]
+        mapLine.append(color)
+        ColorlengthLine.append(length)
+
+        tmpMapColor.append(mapLine)
+        ColorLength.append(ColorlengthLine)
+        #print(mapLine[5])
+
+    print("Finished Compression")
+    print("Drawing Territory and Vertical Borders")
+
+    for y in yRange:
+        #print(drawReader[5,y])
+        if y%256 ==0:
+            print("%g%%"%(y*100/provMap.size[1]))
+        tx = 0
+        for x in range(0,len(tmpMapColor[y])):
+            j=0
+            for l in deffProvList:
+                #print(l)
+                if tmpMapColor[y][x] in l:
+                    for i in range(0,ColorLength[y][x]):
+                        riverMat[tx+i,y] = drawColorList[j]
+                    break
+                j+=1
+            tx+=ColorLength[y][x]
+            #draw ver borders
+            riverMat[tx-1,y] = (0,0,0)
+    
+            
+    #draw hor borders
+    print("Drawing Horizontal Borders")
+    for x in xRange:
+        color = drawReader[x,0]
+        for y in range(1,provMap.size[1]):
+            if drawReader[x,y] == color:
+                pass
+            else:
+                riverMat[x,y] = (0,0,0)
+                color = drawReader[x,y]
+
+    drawingMap.save("BlankMap.png")
+    drawingMap.show()
+
+
+
+
+
+ts1 = time.time()
+readProvinceDeff()
+parssDefaultMap()
+
+deffProvList = []
+impassableProvSea = []
+riverProvList = []
+seaProvList = []
+lakeProvList = []
+wastelandProvList = []
+uninhabitableProvList = []
+impasableterrainProvList = []
+for prov in provList:
+    if prov.id in impassableSeaList or (prov.id in seaList and prov.id in wastelandList):
+        impassableProvSea.append(prov.getRGBA())
+    elif prov.id in riverList:
+        riverProvList.append(prov.getRGBA())
+    elif prov.id in seaList:
+        seaProvList.append(prov.getRGBA())
+    elif prov.id in lakeList:
+        lakeProvList.append(prov.getRGBA())
+    elif prov.id in wastelandList:
+        wastelandProvList.append(prov.getRGBA())
+    elif prov.id in uninhabitableList:
+        uninhabitableProvList.append(prov.getRGBA())
+    elif prov.id in impasableterrainList:
+        impasableterrainProvList.append(prov.getRGBA())
+deffProvList.append(impassableProvSea)
+deffProvList.append(seaProvList)
+deffProvList.append(riverProvList)
+deffProvList.append(lakeProvList)
+deffProvList.append(wastelandProvList)
+deffProvList.append(uninhabitableProvList)
+deffProvList.append(impasableterrainProvList)
+
+
+drawMat(deffProvList)
+ts2 = time.time()
+print("%g Seconds"%(ts2 - ts1))
