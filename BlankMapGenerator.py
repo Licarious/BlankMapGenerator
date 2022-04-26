c6587701df4892ebefadd9e89e2a35d7ff768449
Licarious <37857277+Licarious@users.noreply.github.com>
2022-04-26 03:48:06 -0700

    ProvID Writer
    Add the ability to write the provID to the map.  with flags to determine if to write them and if to exclude water and wasteland.

--- BlankMapGenerator.py
+++ BlankMapGenerator.py
@@ -1,5 +1,5 @@
 
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw 
 import time
 import os
 
@@ -14,24 +14,42 @@ unihabitableColor = (185,185,185,255)       #traversable wastelands (IR)
 impassableColor = (95,95,95,255)            #colorable wastelands (IR)
 borderColor = (0,0,0,255)
 
-drawBorders = True    #wether or not to draw province borders (True/False)
-boldBorders = False    #borders are 2 pixels thick instead of 1 (True/False)
+textColor = (200,50,50,255)
+
+drawBorders = True      #wether or not to draw province borders (True/False)
+boldBorders = False     #borders are 2 pixels thick instead of 1 (True/False)
+
+writeIDs = False             #writes the prov id for each prov to the blank map (True/False)
+ignoreWaterIDs = True       #when writing IDs does not write IDs for Water provs (True/False)
+ignoreWastelandIDs = True   #when writing IDs does not write IDs for Wasteland provs (True/False)
+
 
 
 
 class ProvinceDefinition:
-    id = 0
-    red = 0
-    green = 0
-    blue = 0
-    name = ""
-    name2 = ""
-    other_info = ""
-    lastKnownY = -1
+    def __init__(self):
+        self.id = 0
+        self.red = 0
+        self.green = 0
+        self.blue = 0
+        self.name = ""
+        self.coords = []
+        self.center = (0,0)
     def getRGBA(self):
         return((self.red,self.green,self.blue,255))
     def getRGB(self):
         return((self.red,self.green,self.blue))
+    def __str__(self):
+        return("%i - %s"%(self.id,self.name))
+    def calcCenter(self):
+        l = len(self.coords)
+        totalX = 0
+        totalY = 0
+        if l>0:
+            for c in self.coords:
+                totalX+=c[0]
+                totalY+=c[1]
+            self.center = (int(totalX/l),int(totalY/l))
 
 
 
@@ -138,11 +156,11 @@ def drawMat(deffProvList):
     drawingMap = Image.new("RGBA", (provMap.size[0],provMap.size[1]), color = drawColorList[0])
     #drawingMap.putalpha(0)
     riverMat = drawingMap.load()
-    z=0
-    dis = 5
-    
 
-    count = 0
+    colorIndex = []
+    if writeIDs:
+        for p in provList:
+            colorIndex.append(p.getRGBA())
 
     print("Compressing Image")
     tmpMapColor = []
@@ -178,19 +196,33 @@ def drawMat(deffProvList):
             print("\t%g%%"%(y*100/provMap.size[1]))
         tx = 0
         for x in range(0,len(tmpMapColor[y])):
+            if writeIDs:
+                if tmpMapColor[y][x] in colorIndex:
+                    p = provList[colorIndex.index(tmpMapColor[y][x])]
+                    #print(p)
+                    p.coords.append((int((tx+tx+ColorLength[y][x])/2),y)) #center of compressed line
+                    #for i in range(tx,tx+ColorLength[y][x]):
+                    #    p.coords.append((y,i))
+
             j=1
             for l in deffProvList:
                 #print(l)
                 if tmpMapColor[y][x] in l:
                     for i in range(0,ColorLength[y][x]):
-                        riverMat[tx+i,y] = drawColorList[j]
+                        try:
+                            riverMat[tx+i,y] = drawColorList[j]
+                        except:
+                            pass
                     break
                 j+=1
             tx+=ColorLength[y][x]
             #draw ver borders
             if drawBorders:
                 if not tx == provMap.size[0]:
-                    riverMat[tx-1,y] = borderColor
+                    try:
+                        riverMat[tx-1,y] = borderColor
+                    except:
+                        pass
                     if boldBorders:
                         riverMat[tx-2,y] = borderColor
                     
@@ -208,17 +240,27 @@ def drawMat(deffProvList):
                     pass
                 else:
                     riverMat[x,y] = borderColor
-                    if boldBorders:
+                    if boldBorders and not y < 1:
                         riverMat[x,y-1] = borderColor
                     
                     color = drawReader[x,y]
 
+    if writeIDs:
+        print("drawing ProvIDs")
+        font = ImageFont.truetype("Input\\OpenSans-Semibold.ttf",8)
+        draw = ImageDraw.Draw(drawingMap)
+        for p in provList:
+            if ignoreWaterIDs and (p.getRGBA() in deffProvList[0] or p.getRGBA() in deffProvList[1] or p.getRGBA() in deffProvList[2] or p.getRGBA() in deffProvList[3]):
+                pass
+            elif ignoreWastelandIDs and (p.getRGBA() in deffProvList[4] or p.getRGBA() in deffProvList[5] or p.getRGBA() in deffProvList[6]):
+                pass
+            else:
+                p.calcCenter()
+                draw.text(p.center,"%i"%p.id,textColor,font=font,anchor="mm")
+            
+
     drawingMap.save("BlankMap.png")
     drawingMap.show()
-
-
-
-
 
 ts1 = time.time()
 readProvinceDeff()
